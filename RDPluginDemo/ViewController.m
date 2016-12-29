f2d02cc192a97909f1b0898f6b61e46837a081e6
Radarrrrr <imryd@163.com>
2016-12-29 14:28:22 +0800

    change dylib

--- RDPluginDemo/ViewController.m
+++ RDPluginDemo/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "PluginPlatformManager.h"
+#import "PluginProtocol.h"
+
 #import <Dylib/Dylib.h>
 #import <MyFramework/MyFramework.h>
 #import <PluginOne/PluginOne.h>
+
 
 @interface ViewController ()
 
@@ -21,26 +25,34 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    //----
+    
+    //--------------------
     Person *per = [[Person alloc] init];
     [per run];
     
-    //----
+    
+    //--------------------
     [MyUtils runLibWith:@"我是一个嵌入的动态库" completion:^(NSString *status) {
         NSLog(@"status: %@", status);
     }];
     
-    //----
+    
+    //--------------------
     [OnePrincipal run];
     
     
+    //--------------------
+    NSObject *plugin = [PluginPlatformManager loadFramework:@"Plugin1.framework"];
+    
+    if(plugin && [plugin respondsToSelector:@selector(run)])
+    {
+        [plugin performSelector:@selector(run)];
+    }
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
